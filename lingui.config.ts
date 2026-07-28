d1ac2d8fd5763eb90f1f7c3699b4009ddb97e9eb
ThuanDev <thuan0936a@gmail.com>
2026-07-28 09:51:10 +0700

    chore: update source code

--- lingui.config.ts
+++ lingui.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@lingui/conf";
 import { formatter } from "@lingui/format-po";
 
 export default defineConfig({
-  catalogs: [{ include: ["features"], path: "locales/{locale}/messages" }],
+  catalogs: [{ include: ["src/features"], path: "src/locales/{locale}/messages" }],
   fallbackLocales: { default: "vi" },
   format: formatter({ lineNumbers: false }),
   locales: ["vi", "en"],
